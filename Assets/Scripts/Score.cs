651fe7ce996dfb062eb525a1dff40d29e4e34f34
Yaraslau <ysidarkevich@gmail.com>
2025-01-25 15:44:56 +0100

    score point script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    
+    
+}
---
+++ Assets/Scripts/ScorePoint.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class ScorePoint : MonoBehaviour
+{
+    [SerializeField] private int scoreValue;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
